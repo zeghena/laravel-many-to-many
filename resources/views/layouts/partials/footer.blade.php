dba8738199476f1967231ae0b07476e70121f0b9
davidemarchini <marchini73@gmail.com>
2024-04-15 10:11:40 +0200

    fix

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark text-white">
   <div class="container py-4">
-    &copy; {{ env('APP_AUTHOR', 'Nicola Pio Gallo') }} {{ now()->year }}
+    &copy; {{ env('APP_AUTHOR', 'Davide Marchini') }} {{ now()->year }}
   </div>
 </footer>
